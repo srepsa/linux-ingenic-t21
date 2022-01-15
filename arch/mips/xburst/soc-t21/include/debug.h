245b7201f56413c7c8778cef109fdf9d7f76f629
Your <you@example.com>
2022-01-15 23:00:35 +0100

    jims dirty debug code

--- arch/mips/xburst/soc-t21/include/debug.h
+++ arch/mips/xburst/soc-t21/include/debug.h
@@ -1,6 +1,8 @@
 #ifndef __JIM_DEBUG_H__
 #define __JIM_DEBUG_H__
 
+#define WHEREAMI        pr_info("[DEBUG]: At %s ln. %d (%s)\n", __FILE__, __LINE__, __func__);
+
 void flash_green_led(char count);
 
 #endif /* __JIM_DEBUG_H__ */
